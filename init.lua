49658738ed90de1187fac7cf4f28972d3f938310
Billy S <bbrus@comcast.net>
2019-03-27 19:49:11 -0400

    Removed /tell

--- init.lua
+++ init.lua
@@ -15,12 +15,22 @@ minetest.override_chatcommand("msg", {
       local report = minetest.colorize("lime", name .. " to " .. sendto .. ": " .. message)
       for _, player in ipairs(minetest.get_connected_players()) do
          local n = player:get_player_name()
-         if n ~= name then
+         if n ~= name and n ~= sendto then
             if minetest.check_player_privs(n, {basic_privs = true}) then
                minetest.chat_send_player(n, report)
             end
          end
       end
       return res, reason
+   end
+})
+
+-- Break /tell (I'm tired of it)
+minetest.override_chatcommand("tell", {
+   description = "Old function from the mesecons mod.  Use /msg instead",
+   params = "",
+   privs = {},
+   function = function()
+      return false, "Use /msg instead"
    end
 })
