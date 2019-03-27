6cbc3ee1dcc3a03c4d6046cf20ecffba1bb1bc75
Billy S <bbrus@comcast.net>
2019-03-27 18:07:50 -0400

    First commit

---
+++ init.lua
@@ -0,0 +1,26 @@
+local old_def = minetest.registered_chatcommands["msg"]
+local old_func = minetest.registered_chatcommands["msg"].func
+
+-- Soft dependency on verification so that its override will be loaded first
+minetest.override_chatcommand("msg", {
+   description = "Send a private message (Visible to admins and moderators)",
+   params = old_def.params,
+   privs = old_def.privs,
+   func = function(name, param)
+      res, reason = old_func(name, param)
+      if res == false then
+         return res, reason
+      end
+      local sendto, message = param:match("^(%S+)%s(.+)$") -- Guranateed this will work by oldfunc
+      local report = minetest.colorize("lime", name .. " to " .. sendto .. ": " .. message)
+      for _, player in ipairs(minetest.get_connected_players()) do
+         local n = player:get_player_name()
+         if n ~= name then
+            if minetest.check_player_privs(n, {basic_privs = true}) then
+               minetest.chat_send_player(n, report)
+            end
+         end
+      end
+      return res, reason
+   end
+})
