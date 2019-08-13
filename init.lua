471f98607f3b66de803c17db35944d0d3649aa73
flux <25628292+fluxionary@users.noreply.github.com>
2019-08-13 07:50:54 +0000

    added command to toggle pmview (per-player)

--- init.lua
+++ init.lua
@@ -1,3 +1,18 @@
+local mod_storage = minetest.get_mod_storage()
+
+local function toggle_is_enabled(player_name)
+   local current = mod_storage:get_string(player_name)
+   local new = (current == "disabled") and "enabled" or "disabled"
+   mod_storage:set_string(player_name, new)
+   return new == "enabled"
+end
+
+local function get_is_enabled(player_name)
+   local v = mod_storage:get_string(player_name)
+   return v == "" or v == "enabled"
+end
+
+
 local function get_all_connected_player_names()
    if cloaking ~= nil and cloaking.get_connected_names ~= nil then
       return cloaking.get_connected_names()
@@ -22,11 +37,11 @@ minetest.override_chatcommand("msg", {
       if res == false then
          return res, reason
       end
-      local sendto, message = param:match("^(%S+)%s(.+)$") -- Guranateed this will work by oldfunc
+      local sendto, message = param:match("^(%S+)%s(.+)$") -- Guaranteed this will work by oldfunc
       local report = minetest.colorize("lime", name .. " to " .. sendto .. ": " .. message)
       for _, n in ipairs(get_all_connected_player_names()) do
          if n ~= name and n ~= sendto then
-            if minetest.check_player_privs(n, {basic_privs = true}) then
+            if minetest.check_player_privs(n, {basic_privs = true}) and get_is_enabled(n) then
                minetest.chat_send_player(n, report)
             end
          end
@@ -46,3 +61,16 @@ if minetest.registered_chatcommands["tell"] then
       end
    })
 end
+
+minetest.register_chatcommand("toggle_pmview", {
+   privs = {basic_privs = true},
+   description = "toggle whether you will see pmview messages",
+   func = function(caller, params)
+      local is_enabled = toggle_is_enabled(caller)
+      if is_enabled then
+         return true, "pmview enabled"
+      else
+         return true, "pmview disabled"
+      end
+   end
+})
